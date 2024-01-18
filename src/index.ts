0f010eaec56d79bc6608b10db88e2dc362f94d1d
Kai Aldag <kaialdag@icloud.com>
2024-01-18 15:57:39 -0800

    update typechain and ts conf

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // export { default as JasminePoolFactoryABI } from "../abi/contracts/JasminePoolFactory.sol/JasminePoolFactory.json";
 // export { default as JasmineRetirementServiceABI } from "../abi/contracts/JasmineRetirementService.sol/JasmineRetirementService.json";
 // Typechain Exports
-export * from "../typechain";
+export * from "../typechain/contracts";
 // export { JasminePool__factory as JasminePoolContractFactory } from "../typechain/factories/contracts/JasminePool__factory";
 // export { JasminePoolFactory__factory as JasminePoolFactoryContractFactory } from "../typechain/factories/contracts/JasminePoolFactory__factory";
 // export { JasmineRetirementService__factory as JasmineRetirementServiceContractFactory } from "../typechain/factories/contracts/JasmineRetirementService__factory";
