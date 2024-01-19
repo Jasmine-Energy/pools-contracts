cb73a5efcef3005f4c29ad77e4a9721acfadff2a
Kai Aldag <kaialdag@icloud.com>
2024-01-18 16:10:49 -0800

    use requires...

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,31 @@
 // export { default as JasminePoolFactoryABI } from "../abi/contracts/JasminePoolFactory.sol/JasminePoolFactory.json";
 // export { default as JasmineRetirementServiceABI } from "../abi/contracts/JasmineRetirementService.sol/JasmineRetirementService.json";
 // Typechain Exports
-export * from "../typechain/contracts";
+const typechainContracts = require("../typechain/contracts");
+const typechainFactories = require("../typechain/factories/contracts");
+
 // export { JasminePool__factory as JasminePoolContractFactory } from "../typechain/factories/contracts/JasminePool__factory";
 // export { JasminePoolFactory__factory as JasminePoolFactoryContractFactory } from "../typechain/factories/contracts/JasminePoolFactory__factory";
 // export { JasmineRetirementService__factory as JasmineRetirementServiceContractFactory } from "../typechain/factories/contracts/JasmineRetirementService__factory";
 // Utils Exports
 // export * as utils from "../utils";
-export * from "../utils";
+const utils = require("../utils");
+// export * from "../utils";
 // Types
 // export * as types from "../types";
-export * from "../types";
+// export * from "../types";
+const types = require("../types");
 
-import "../hardhat/plugin";
+// import "../hardhat/plugin";
+const plugin = require("../hardhat/plugin");
 // Deploy Scripts
+
+module.exports = {
+  typechain: {
+    contracts: typechainContracts,
+    factories: typechainFactories,
+  },
+  utils,
+  types,
+  plugin,
+};
